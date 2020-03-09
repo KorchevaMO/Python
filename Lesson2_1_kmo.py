aaa47a40a5b4a2184ce7bf3d9090d94215e84de6
korchevamo <titova-m-o@yandex.ru>
2020-03-09 13:57:14 +0300

    task

---
+++ Lesson2_1_kmo.py
@@ -0,0 +1,17 @@
+my_list = [1]
+my_list.append('new_el')
+my_list.append(2)
+my_list.append(35.8)
+my_list.append(333)
+#i = 0
+print(type(my_list.pop(0)))
+print(type(my_list.pop(1)))
+print(type(my_list.pop(2)))
+print(type(my_list.pop(3)))
+print(type(my_list.pop(4)))
+#while i <= len(my_list):
+      #print(type(my_list.pop(i))
+       #i + = i
+
+
+
