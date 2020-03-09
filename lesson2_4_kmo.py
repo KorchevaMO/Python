181dc60ffd727b6c927cc99af7639c652fd050e5
korchevamo <titova-m-o@yandex.ru>
2020-03-09 18:44:13 +0300

    до конца не отладила

---
+++ lesson2_4_kmo.py
@@ -0,0 +1,7 @@
+
+#for word in input('Введите слова через пробел :').split():
+   # print(word)
+word = input('Введите слова через пробел :').split()
+for idx, itm in enumerate(word):
+    print(idx,':',word)
+
